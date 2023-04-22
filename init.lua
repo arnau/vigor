7403ca2fcdfaa53c724cf5077bf98ba9b949727e
Arnau Siches <asiches@gmail.com>
2023-04-22 14:28:58 +0100

    chore: Disable minimap by default
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- init.lua
+++ init.lua
@@ -171,6 +171,6 @@ vim.keymap.set('n', '<c-j>', climber.swap_next, keyopts)
 --
 --
 -- minimap
-vim.g.minimap_width = 10
-vim.g.minimap_auto_start = 1
-vim.g.minimap_auto_win_enter = 1
+-- vim.g.minimap_width = 10
+-- vim.g.minimap_auto_start = 1
+-- vim.g.minimap_auto_win_enter = 1
