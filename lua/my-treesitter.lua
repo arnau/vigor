fc3078cff37bf96519134a0785e2c2195cb7de2e
Arnau Siches <asiches@gmail.com>
2023-07-03 08:12:48 +0100

    feat: Add tree-sitter-csv
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- lua/my-treesitter.lua
+++ lua/my-treesitter.lua
@@ -23,4 +23,27 @@ treesitter.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
+
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    }
+  },
+}
+
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+parser_config.csv = {
+  install_info = {
+    url = "https://github.com/arnau/tree-sitter-csv",
+    files = {"src/parser.c"},
+    branch = "main",
+    generate_requires_npm = false,
+    requires_generate_from_grammar = false,
+  },
+  filetype = "csv",
 }
